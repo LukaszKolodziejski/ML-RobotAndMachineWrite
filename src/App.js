9d9851d6a9fbb15663c1e34740170d9ded96bb7f
Łukasz Kołodziejski <lukasz-kolodziejski3333@wp.pl>
2020-05-31 12:59:32 +0200

    change 5

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ import styles from "./css/App.module.css";
 const App = () => {
   return (
     <div className={styles.App}>
-      {/* <Robot /> */}
+      <Robot />
       <Typing />
     </div>
   );

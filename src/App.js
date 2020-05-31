e111d539b9a75149f87360f0019d0bead50083f0
Łukasz Kołodziejski <lukasz-kolodziejski3333@wp.pl>
2020-05-31 09:44:50 +0200

    fix damaged old project

--- src/App.js
+++ src/App.js
@@ -1,24 +1,15 @@
 import React from "react";
-import logo from "./logo.svg";
-import "./App.css";
+import Robot from "./components/Robot";
+import Typing from "./components/Typing";
+import styles from "./css/App.module.css";
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>this is my new website in React.js</p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className={styles.App}>
+      <Robot />
+      <Typing />
     </div>
   );
-}
+};
 
 export default App;

0e40a3f1ec79f770c177e4c76efac380e0b0105b
Łukasz Kołodziejski <lukasz-kolodziejski3333@wp.pl>
2020-05-31 12:36:42 +0200

    Updates

---
+++ precache-manifest.452758ce338a0fa3be2c6ec1672fece3.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5a6eb4fe33d6cf7b38367defe25b3c27",
+    "url": "/ML-RobotAndMachineWrite/index.html"
+  },
+  {
+    "revision": "f53472ea8b4c70b2774e",
+    "url": "/ML-RobotAndMachineWrite/static/css/main.04986193.chunk.css"
+  },
+  {
+    "revision": "2f6f41b3344befe8812a",
+    "url": "/ML-RobotAndMachineWrite/static/js/2.106b09af.chunk.js"
+  },
+  {
+    "revision": "0e4e3b6e9d7592356c4f61666708460a",
+    "url": "/ML-RobotAndMachineWrite/static/js/2.106b09af.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f53472ea8b4c70b2774e",
+    "url": "/ML-RobotAndMachineWrite/static/js/main.66c7d75d.chunk.js"
+  },
+  {
+    "revision": "afaa0d7402111e670f39",
+    "url": "/ML-RobotAndMachineWrite/static/js/runtime-main.f7ea01bd.js"
+  },
+  {
+    "revision": "711dcbaabb8e04f3acf14671e0df9045",
+    "url": "/ML-RobotAndMachineWrite/static/media/robot.711dcbaa.svg"
+  }
+]);
